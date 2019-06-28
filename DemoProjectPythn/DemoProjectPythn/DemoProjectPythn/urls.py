27a625e053c7fd1b604dd461e49c26ad2c111669
shubh009 <gupta.shubhanshu007@gmail.com>
2019-06-28 23:11:58 +0530

    Add files via upload

---
+++ DemoProjectPythn/DemoProjectPythn/DemoProjectPythn/urls.py
@@ -0,0 +1,51 @@
+﻿"""
+Definition of urls for DemoProjectPythn.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+from . import  settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^about', 'app.views.about', name='about'),
+      url(r'^CourseList', 'app.views.CourseList', name='CourseList'),
+       url(r'^CourseDetails', 'app.views.CourseDetails', name='CourseDetails'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ DemoProjectPythn/DemoProjectPythn/app/views.py
@@ -0,0 +1,77 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def CourseList(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/CourseList.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Course List',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def CourseDetails(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/CourseDetails.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Course Details',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
